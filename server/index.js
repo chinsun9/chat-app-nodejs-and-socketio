ea15028d0cfa22c2717da703b7d927c098377d4f
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-10-23 09:15:55 +0900

    Update npm scripts

--- server/index.js
+++ server/index.js
@@ -7,14 +7,13 @@ const io = require('socket.io')(http);
 const favicon = require('express-favicon');
 const path = require('path');
 
-const port = 5500;
+const port = normalizePort(process.env.PORT || 5500);
 
 http.listen(port, () => {
-  console.log(`Server is listening on port ${port}.`);
+  console.log(`Server is listening on port http://localhost:${port}.`);
 });
 
 app.use(favicon(path.join(__dirname, '../public/favicon.ico')));
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/ping', (req, res) => {
@@ -24,11 +23,26 @@ app.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
+function normalizePort(val) {
+  const port_int = parseInt(val, 10);
+
+  if (isNaN(port_int)) {
+    return val;
+  }
+
+  if (port_int >= 0) {
+    return port_int;
+  }
+
+  return false;
+}
+
 ////////
 
 const users = {};
 
 io.on('connection', (socket) => {
+  console.log(socket);
   socket.on('new-user', (name) => {
     users[socket.id] = name;
     socket.broadcast.emit('user-connected', name);
